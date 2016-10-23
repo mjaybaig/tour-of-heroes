43b69df5ff7f79634c6cc2d80e993c1ed9cb33db
M. Jameel Baig <mjameelbaig@hotmail.com>
2016-10-24 00:50:19 +0500

    where will this go

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
 
+export class Hero{
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `<h1>{{title}}</h1>
+                <h2>{{hero}} details!</h2>`
 })
-export class AppComponent { }
+export class AppComponent {
+    title = "Tour of Heroes";
+    hero = "Windstorm";
+ }
